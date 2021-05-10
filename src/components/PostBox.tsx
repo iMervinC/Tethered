cde43230043139db4320e20ab6dcd22d8a3a6767
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-05-10 21:24:34 +0800

    highlight Post

--- src/components/PostBox.tsx
+++ src/components/PostBox.tsx
@@ -4,9 +4,20 @@ import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { PostHeader } from '@/components/UI'
 import { Post } from '@/utils/types'
 
-const PostBox: FC<Post> = ({ username, body, createdAt, likes, comments }) => {
+interface PostT extends Post {
+  cb?: (e: any) => void
+}
+
+const PostBox: FC<PostT> = ({
+  username,
+  body,
+  createdAt,
+  likes,
+  comments,
+  cb,
+}) => {
   return (
-    <li className="grid-home__item">
+    <li className="grid-home__item" onClick={cb}>
       <PostHeader name={username} date={createdAt} />
       <span className="grid-home__item__content scroll">{body}</span>
       <div className="grid-home__item__reactions">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react'
 import { PostBox, PostCreate, PostLoader } from '@/components'
 import { Layout } from '@/components/Wrappers'
 import { useAllPost } from '@/hooks/PostHooks'
+import { Post } from '@/utils/types'
 
 const Home = () => {
   const { data, error, loading } = useAllPost()
-
+  const [togglePost, setTogglePost] = useState<Post | null>(null)
   return (
-    <Layout title="Welcome" auth>
-      <ul className="grid-home">
-        <PostCreate key="Create" />
-        {loading && <PostLoader />}
-        {data?.getPosts.map((post) => (
-          <PostBox key={post.id} {...post} />
-        ))}
-      </ul>
-    </Layout>
+    <>
+      {togglePost && (
+        <div
+          className="post-highlight"
+          onClick={() => {
+            setTogglePost(null)
+          }}
+        >
+          <PostBox {...togglePost!} cb={(e) => e.stopPropagation()} />
+        </div>
+      )}
+      <Layout title="Home" auth>
+        <ul className="grid-home">
+          <PostCreate key="Create" />
+          {loading && <PostLoader />}
+          {data?.getPosts.map((post) => (
+            <PostBox key={post.id} {...post} cb={() => setTogglePost(post)} />
+          ))}
+        </ul>
+      </Layout>
+    </>
   )
 }
 
